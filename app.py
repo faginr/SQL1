7938b70b27c0a0630cb396c8923c42653820d2f1
faginr <rexwfagin@gmail.com>
2022-02-24 20:22:54 -0800

    Finished tutorial

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, redirect, request, flash, url_for
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row #name based access to columns
     return conn
 
 app = Flask(__name__)
@@ -14,6 +14,9 @@ app.config['SECRET_KEY'] = 'NalaBear26!'
 def index():
     conn = get_db_connection()
     todos = conn.execute('SELECT i.content, l.title FROM items i JOIN lists l ON i.list_id = l.id ORDER BY l.title;').fetchall()
+    # Get content of item and title of the list it belongs to
+    # by joining both items and lists tables. Match list_id from items
+    # table to id in lists table
 
     lists = {}
     print(todos)
@@ -26,3 +29,27 @@ def index():
 
     conn.close()
     return render_template('index.html', lists = lists)
+
+@app.route('/create/', methods=('GET','POST'))
+def create():
+    conn = get_db_connection()
+
+    if request.method == 'POST':
+        content = request.form['content']
+        list_title = request.form['list']
+
+        if not content:
+            flash('Content is required')
+            return redirect(url_for('index'))
+        
+        list_id = conn.execute('SELECT id FROM lists WHERE title = (?);', (list_title,)).fetchone()['id'] # get list id from provided title
+        conn.execute('INSERT INTO items (content, list_id) VALUES (?,?)', (content, list_id)) # insert new todo item into items table
+
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+
+    lists = conn.execute('SELECT title FROM lists;').fetchall()
+
+    conn.close()
+    return render_template('create.html', lists=lists)
--- list_example.py
+++ list_example.py
@@ -6,6 +6,10 @@ todos = conn.execute('SELECT i.content, l.title FROM items i JOIN lists l ON i.l
 
 lists = {}
 
+# groupby will go through each item in the todos variable
+# which is everything returned from the sql select
+# k is list titles
+# g is the group that contains the to-do items of each list title.
 for k,g in groupby(todos, key=lambda t: t['title']):
     lists[k] = list(g)
 
